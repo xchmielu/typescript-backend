cbd5b1cc3f3ad3c1f38261ce5978b5394235700f
xchmielu <mchmielewski989@gmail.com>
2020-02-16 15:49:39 +0100

    editted scripts for dev and test ENV

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,12 @@
 import { ConnectionOptions } from 'mongoose';
 
+const { NODE_ENV = 'dev' } = process.env;
+console.log(NODE_ENV);
 const {
     MONGO_USERNAME = 'admin',
     MONGO_PASSWORD = 'secret',
     MONGO_HOST = 'localhost',
-    MONGO_PORT = 27017,
+    MONGO_PORT = NODE_ENV === 'testing' ? 27017 : 27018,
     MONGO_DATABASE = 'auth'
 } = process.env;
 
